5d532b9632bb6b9bd165e692ddc93b1a37688fb3
Enzo Watanabe <enzowatanabe@hotmail.com>
2025-07-02 09:04:22 -0300

    "termino de ajuste"

--- src/app/api/contasapagar/[id]/route.ts
+++ src/app/api/contasapagar/[id]/route.ts
@@ -8,7 +8,7 @@ export async function PUT(req: NextRequest) {
     const { pathname } = req.nextUrl;
     const id = Number(pathname.split('/').pop());
     const data = await req.json();
-    const conta = await prisma.ContaAPagar.update({
+    const conta = await prisma.contaAPagar.update({
       where: { id },
       data,
     });
@@ -22,7 +22,7 @@ export async function DELETE(req: NextRequest) {
   try {
     const { pathname } = req.nextUrl;
     const id = Number(pathname.split('/').pop());
-    await prisma.ContaAPagar.delete({ where: { id } });
+    await prisma.contaAPagar.delete({ where: { id } });
     return NextResponse.json({ ok: true });
   } catch (error) {
     return NextResponse.json({ error: 'Erro ao excluir conta.' }, { status: 500 });
